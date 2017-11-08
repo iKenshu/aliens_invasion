1533bd0db1ade9dbe5d38411f6dd42ad5d1243e6
Kevin Morales <undefined.hlo.o@gmail.com>
2017-11-08 08:12:31 -0400

    play button

--- game_functions.py
+++ game_functions.py
@@ -46,10 +46,16 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """ Update images on the screen and flip to the new screen."""
+
+    # Draw the play button if the game is inactive
+    if not stats.game_active:
+        play_button.draw_button()
+
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+
     # Redraw all bullets behind ship and aliens
     for bullet in bullets.sprites():
         bullet.draw_bullet()
